2da06ee53a10abd77f51fed66594be23e045ad59
ircfantasy <ircfantasy@gmail.com>
2023-06-02 22:09:23 +0400

    wip

--- src/main/java/automatron/utils/Config.java
+++ src/main/java/automatron/utils/Config.java
@@ -3,7 +3,7 @@ package automatron.utils;
 import lombok.Getter;
 
 @Getter
-public class Config {  //todo rework to application.properties
+public class Config { //todo rework to application.properties
 
     public static final String baseUrl = "http://localhost:18080/api"; // jenkins runs on 8080
 
