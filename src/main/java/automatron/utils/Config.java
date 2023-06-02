f297e55f434d5a38ba802e6b3275464712d44e15
ircfantasy <puzankov@protei.ru>
2023-06-02 22:03:15 +0400

    wip

--- src/main/java/automatron/utils/Config.java
+++ src/main/java/automatron/utils/Config.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public class Config {  //todo rework to application.properties
 
-    public static final String baseUrl = "http://localhost:8080/api";
+    public static final String baseUrl = "http://localhost:18080/api"; // jenkins runs on 8080
 
     public static String authUrl = baseUrl + "/accounting/login";
 
